eddd06117fda8382d0354259282f23c4aba7ec2d
Bruce Hsu <brucehsu1126@gmail.com>
2023-01-31 10:56:32 -0800

    changed initial render to render 5 day as well, and added celsius to current weather units

--- src/UI.js
+++ src/UI.js
@@ -6,7 +6,7 @@ import FutureWeather from './nextWeekWeather'
 
 const UI = function createUI (){
   // changes all weather data for today
-  const renderCurrentWeatherData = function changeCurrentWeatherDataToMatchDOM(currWeatherData){
+  const renderCurrentWeatherData = function changeCurrentWeatherDataToMatchDOM(currWeatherData, isFahrenheit){
     // gets all the elements that need to be changed
     const currentWeatherDOM = document.querySelector('[data-weather]');
     const currentLocationDOM = document.querySelector('[data-location]');
@@ -20,11 +20,19 @@ const UI = function createUI (){
     currentWeatherDOM.innerText = currWeatherData.description;
     currentLocationDOM.innerText = currWeatherData.location;
     currentTimeDOM.innerText = currWeatherData.time;
-    currentTemperature.innerText = `${convertUnits.kToF(currWeatherData.temperature)}°F`;
-    currentHighTemperature.innerText = `High: ${convertUnits.kToF(currWeatherData.max_temperature)}°F`;
-    currentLowTemperature.innerText = `Low: ${convertUnits.kToF(currWeatherData.min_temperature)}°F`;
     currentHumidity.innerText = `Humidity: ${currWeatherData.humidity}%`;
-    currentWind.innerText = `Wind: ${convertUnits.mToMi(currWeatherData.wind_speed)} mph`;
+
+    if(isFahrenheit){
+      currentTemperature.innerText = `${convertUnits.kToF(currWeatherData.temperature)}°F`;
+      currentHighTemperature.innerText = `High: ${convertUnits.kToF(currWeatherData.max_temperature)}°F`;
+      currentLowTemperature.innerText = `Low: ${convertUnits.kToF(currWeatherData.min_temperature)}°F`;
+      currentWind.innerText = `Wind: ${convertUnits.mToMi(currWeatherData.wind_speed)} mph`;
+    } else {
+      currentTemperature.innerText = `${convertUnits.kToC(currWeatherData.temperature)}°C`;
+      currentHighTemperature.innerText = `High: ${convertUnits.kToC(currWeatherData.max_temperature)}°C`;
+      currentLowTemperature.innerText = `Low: ${convertUnits.kToC(currWeatherData.min_temperature)}°C`;
+      currentWind.innerText = `Wind: ${currWeatherData.wind_speed} m/s`;      
+    }
   }
 
   function remove5DayWeatherData (){
@@ -74,18 +82,28 @@ const UI = function createUI (){
     })
   }
 
-  function initialRender(){
-    addLocationFormFunctionality();
+  function addChangeMetricBtn(currWeatherData, next5DayWeatherData){
+    const changeMetricBtn = document.getElementById('change-units-btn');
+    changeMetricBtn.addEventListener('click', function(){
+      next5DayWeatherData.forEach(data => data.toggleUnits())
+    })
+    const isFahrenheit = next5DayWeatherData.isFahreheit;
+    renderCurrentWeatherData(currWeatherData, isFahrenheit);
+    render5DayWeatherData(next5DayWeatherData);
+  }
 
-    // initially render San Francisco
-    const currWeatherDataPromise = getWeatherData(
-      {city: "San Francisco"}
-    );
+  function initialRender(locationObj){
+    addLocationFormFunctionality();
+    
+    const currWeatherDataPromise = getWeatherData(locationObj);
+    const next5DayWeatherDataPromise = get5DayWeatherData(locationObj);
     // change the promise to an object
     currWeatherDataPromise.then((currWeatherData) => {
-      renderCurrentWeatherData(currWeatherData);
+      renderCurrentWeatherData(currWeatherData, true);
     });
-
+    next5DayWeatherDataPromise.then((fiveDayWeatherData) => {
+      render5DayWeatherData(fiveDayWeatherData);
+    });
   }
 
   function renderNewLocation(weatherData){
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import UI from './UI'
 
 // creates the weather page
 const main = function doAllOperations(){
-  UI.initialRender();
+  const initialLocation = {
+    city: 'San Francisco'
+  }
+  UI.initialRender(initialLocation);
 }
 
 main();
